8ab3535a80fb65ee77ee03ac750ad80dd4cda7c3
alan zambrano <alan.zambrano@diginteligencia.com>
2016-08-10 17:49:14 -0500

    First Commit

---
+++ src/components/VideoComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class VideoComponent extends React.Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    )
+  }
+};
+
+export default VideoComponent;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import VideoComponent from './components/VideoComponent';
+
+reactDom.render(<VideoComponent />, document.getElementById('myApp'));
